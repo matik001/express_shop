fdf997b2d828950620485a6f843114e7e15b7df2
Mati <mateusz.kisiel.mk@gmail.com>
2022-01-12 17:30:55 +0100

    Removed dist

--- dist/middleware/isAuth.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (function (req, res, next) {
-    if (!req.user) {
-        return res.redirect("/login?returnUrl=" + req.url);
-    }
-    next();
-});
-//# sourceMappingURL=isAuth.js.map
--- dist/routes/clientRouter.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var clientController_1 = require("../controllers/clientController");
-var clientRouter = express_1.Router();
-clientRouter.get('/', clientController_1.getIndex);
-exports.default = clientRouter;
-//# sourceMappingURL=clientRouter.js.map
